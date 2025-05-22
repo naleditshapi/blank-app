4323f8dd3398edb582366156fffc3ba31193b2e0
Naledi Tshapi <114586394+naleditshapi@users.noreply.github.com>
2025-05-22 09:01:51 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.text_input("Enter your name")
+studymethod = st.selectbox ("StudyMethod", "Crammer", "Pre-Planner")
+st.button("Diva")
+
+if button:
+    with st.spinner("Loading your divs stastud"):
+        if studymethod
